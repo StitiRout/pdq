d7a7cbaefee75908f35b4089989e54b2364e8f66
StitiRout <stitirout2024@gmail.com>
2025-09-22 11:04:18 +0530

    functional recursion

--- dsa11.py
+++ dsa11.py
@@ -1,9 +1,17 @@
-# Functional Recursion
+
 # sum of 1 to n
 
-def fxn(sum,a,b):
-    if a>b:
-        print(sum)
-        return
-    fxn(sum+a,a+1,b)
-fxn(0,1,10)
+# def fxn(sum,a,b):
+#     if a>b:
+#         print(sum)
+#         return
+#     fxn(sum+a,a+1,b)
+# fxn(0,1,10)
+
+#Functional Recursion
+# F(N)=N+F(N-1)
+def fxn2(n):
+    if n==1:
+        return 1
+    return n+fxn2(n-1)
+print(fxn2(10))
